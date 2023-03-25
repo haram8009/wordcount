4492cf90b7a9cd6890177cd7f3ac81be62b5539f
haram8009 <haram8009@gmail.com>
2023-03-25 22:59:09 +0900

    nltk 추가

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 import re
 from konlp.kma.klt2023 import klt2023
+import nltk
 
 def home(request):
     return render(request, 'home.html')
@@ -11,6 +12,11 @@ def KLT2023_Nlist(text):
     klt = klt2023()
     list = klt.nouns(text) 
     return list
+
+def get_nltk_list(text):
+    nltk.download('punkt')
+    words = nltk.tokenize.word_tokenize(text) # 토큰화한 단어들 리스트로 반환
+    return words
 
 def result(request):
     text = request.GET['text'].strip()
@@ -43,6 +49,17 @@ def result(request):
             klt_word_dict[word] = 1
     klt_words = sorted(klt_word_dict.items(),key=lambda x:x[1], reverse=True)
     # klt2023 ver 총 단어 개수
-    klt_total_count = len(klt_word_dict)
+    klt_total_count = len(klt_list)
+    # nltk ver
+    nltk_list = get_nltk_list(text)
+    nltk_word_dict = {}
+    for word in nltk_list:
+        if word in nltk_word_dict:
+            nltk_word_dict[word] += 1
+        else:
+            nltk_word_dict[word] = 1
+    nltk_words = sorted(nltk_word_dict.items(),key=lambda x:x[1], reverse=True)
+    # nltk ver 총 단어 개수
+    nltk_total_count = len(nltk_list)
 
-    return render(request, "result.html", {"simple_length":simple_length,'simple_words':simple_words,"simple_total_count":simple_total_count,"replaced_length":replaced_length, "klt_words":klt_words, "klt_total_count":klt_total_count})
+    return render(request, "result.html", {"simple_length":simple_length,'simple_words':simple_words,"simple_total_count":simple_total_count,"replaced_length":replaced_length, "klt_words":klt_words, "klt_total_count":klt_total_count, "nltk_words":nltk_words,"nltk_total_count":nltk_total_count})
